dd7af146d86d17468434a32381dd3906acb53087
megh974 <hilbert.m@northeastern.edu>
2026-06-30 08:47:24 -0600

    Practicing writing code in R and committing to Git during lab meeting

---
+++ scripts/Meg_scripts_practicingcode.R
@@ -0,0 +1,13 @@
+# Summer 2026 practicing R and Git
+# created Jun 2026 Meg Hilbert
+
+# download libraries----
+library(tidyverse)
+
+# creating objects----
+x<- 1
+x1<-c(1,2,3,4)
+x2<-c("1","2","3","4") # quotes make it a character
+
+# practice making a dataframe
+x3<- data.frame(var1=seq(1,10,1), var2=("a"), var3=c(1.2,3.4))
