9db3e7c92fff0516977e1e705d496c86bcf99c4f
Summer <szamora7308@gmail.com>
2026-07-13 16:21:02 -0500

    updated 7/7

---
+++ scripts/summer_script1_examplescript.R
@@ -0,0 +1,9 @@
+#summerz - created 6/30/26
+#loading tidyverse package
+library(tidyverse)
+#example objects
+x<-1
+x1<-c(1,2,3,4,5)
+x2<-c("1",2,3,4,5)
+#making data frame
+x3<-data.frame(var1=seq(1,10,1), var2=("a"),var3=c(1.2,3.4))
