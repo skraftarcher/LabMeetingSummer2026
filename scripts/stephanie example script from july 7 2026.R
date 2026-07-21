0ae879f0c8c5486a1b352f2228d58dc5cdd6b626
skraftarcher <sarcher@lumcon.edu>
2026-07-21 10:00:47 -0500

    example for joining code and an ugly figure

--- scripts/stephanie example script from july 7 2026.R
+++ scripts/stephanie example script from july 7 2026.R
@@ -60,3 +60,67 @@ ggplot(data=biod%>%
   geom_point(aes(x=date.retrieved,y=abundance),color="red",size=6)+
   geom_line(aes(x=date.retrieved,y=abundance,group=tray))
 
+# joining salinity data to biodiversity 
+# first you have to organize the salinity data
+
+# look at the structure of the salinity data
+ggplot(data=sal)+
+  geom_line(aes(x=date,y=PSU,group=site))+
+  facet_wrap(~site)
+
+
+# make a dataset from biodiversity data to get site, start, and end date/times
+deploys<-biod%>%
+  select(site,end=date.retrieved)%>%
+  distinct()%>%
+  mutate(start=end-days(x=14),
+         start=ymd_hms(paste(start,"18:00:00")),
+         end=ymd_hms(paste(end,"8:00:00")),
+         date.int=interval(start,end),
+         deploy=paste0(site,".",row.names(.)))
+
+
+# find intervals the salinity date goes with
+# first thing we have to do is turn date into a date/time object
+sal2<-sal%>%
+  separate(date,into=c("date","time"),sep=" ")%>%
+  mutate(time=ifelse(is.na(time),"00:00:00",time),
+         date.time=ymd_hms(paste(date,time)))%>%
+  select(-date,-time)
+
+sal2$deploy<-NA
+
+
+# now figure out how to link the intervals in deploys
+for(i in 1:nrow(sal2)){
+  t1<-sal2$date.time[i]
+  dep2<-filter(deploys,site==sal2$site[i])
+  t2<-dep2$deploy[t1 %within% dep2$date.int]
+  if(length(t2)>0){
+    sal2$deploy[i]<-t2
+  }
+  print(i)
+}
+
+
+# summarize salinity data 
+sal3<-sal2%>%
+  filter(!is.na(deploy))%>%
+  group_by(deploy)%>%
+  summarize(mean.sal=mean(PSU,na.rm=T),
+            median.sal=median(PSU,na.rm=T),
+            sd.sal=sd(PSU,na.rm=T),
+            min.sal=min(PSU,na.rm=T),
+            max.sal=max(PSU,na.rm = T))
+
+dep.sal<-left_join(deploys,sal3)%>%
+  mutate(date.retrieved=ymd(paste(year(end),month(end),day(end))))%>%
+  select(-start,-date.int,-deploy,-end)
+
+
+biod.withenv<-left_join(biod,dep.sal)
+
+# example ugly plot
+ggplot(data=biod%>%mutate(x2=row.names(.)))+
+  geom_point(aes(x=x2,y=wet.weight,color=site,alpha=date.retrieved),size=4)+
+  scale_y_continuous(n.breaks=100)
